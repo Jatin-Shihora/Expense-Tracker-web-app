0b9955f35abef09e02fe219288f5e82f3f8efc75
Jatin Shihora <jatinshihora0123@gmail.com>
2021-11-07 19:02:38 +0530

    Code refactoring

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,10 @@
-import React,{useState} from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 import "./ExpenseItem.css";
 
 const ExpenseItem=(props)=> {
-  //Destructuring of the array
-  const [title,setTitle] = useState(props.title)
-
-  const clickHandler = () => {
-    setTitle('Updated!')
-    console.log(title)
-  };
 
   return (
     <Card className='expense-item'>
@@ -19,10 +12,9 @@ const ExpenseItem=(props)=> {
       between the opening and closing tag */}
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
