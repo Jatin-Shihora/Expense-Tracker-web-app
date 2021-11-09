fe976e92470e6d92e7956dff47953385ddf60c7b
Jatin Shihora <jatinshihora0123@gmail.com>
2021-11-09 18:20:24 +0530

    Resizing of ExpenseForm using conditional content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
-import './NewExpense.css';
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const saveExpenseDataHandler=(enteredExpenseData)=>{
-      const expenseData={
-        ...enteredExpenseData,
-        id : Math.random().toString()
-      }
-      props.onAddExpense(expenseData)
-  }
+  const [isEditing, setIsEditing] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false)
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+    <div className="new-expense">
+      {/* Another way of writing the conditional content */}
+      {/* if cancel or a expense gets added than show the add expenses button */}
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expenses</button>
+      )}
+      {/* If add Expenses button gets clicked than show the form */}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
